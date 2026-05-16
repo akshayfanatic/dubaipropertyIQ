343e9b0d11f6c6186c30a0f4ba1a608527c44808
akshayfanatic <akshay@fanaticcoders.com>
2026-05-16 10:21:40 +0530

    fix(config) : added new origin for testing

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
         hostname: supabaseHostname ?? '',
         pathname: '/storage/v1/object/public/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
     ],
   },
 
